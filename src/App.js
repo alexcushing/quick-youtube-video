3901e831830aad02ac6d6a149f404726f04cf516
Alex Cushing <alexander_cushing@student.uml.edu>
2017-04-02 17:39:15 -0400

    styled a little

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
           className="input"
           placeholder="video URL"
         />
-        <button type="submit" onClick={this.submit}> submit </button>
+        <button type="submit" onClick={this.submit}> search </button>
         {
           this.state.search ? 
           <div>
